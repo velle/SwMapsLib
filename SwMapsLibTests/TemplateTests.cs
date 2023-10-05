97c886a44224467b087b8e2f92aae081802be431
thomas <mrvelle@gmail.com>
2023-10-05 17:31:04 +0200

    commented out test in TemplateTests, because of missing data file SWMaps_GPRS_Master.swmr

--- SwMapsLibTests/TemplateTests.cs
+++ SwMapsLibTests/TemplateTests.cs
@@ -12,17 +12,19 @@ namespace SwMapsLibTests
 	[TestClass]
 	public class TemplateTests
 	{
-		[TestMethod]
-		public void ReadV2Template()
-		{
-			var path = Path.Combine("Data", "SWMaps_GPRS_Master.swmr");
-			var reader = new TemplateReader(path);
-			var template = reader.Read();
-
-			var writer = new TemplateV2Writer(template);
-			var outPath = Path.Combine("Data", "SWMaps_GPRS_Master_re.swmt");
-			writer.WriteTemplate(outPath);
-		}
+		// SWMaps_GPRS_Master.swmr is missing in repo
+		//
+		// [TestMethod]
+		// public void ReadV2Template()
+		// {
+		// 	var path = Path.Combine("Data", "SWMaps_GPRS_Master.swmr");
+		// 	var reader = new TemplateReader(path);
+		// 	var template = reader.Read();
+        //
+		// 	var writer = new TemplateV2Writer(template);
+		// 	var outPath = Path.Combine("Data", "SWMaps_GPRS_Master_re.swmt");
+		// 	writer.WriteTemplate(outPath);
+		// }
 
 		[TestMethod]
 		public void ConvertV1ToV2()
